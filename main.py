cc20eaac7ce0f128b6c3077b3a0a6e0ee3e03bee
doing_gyu <srdyhtty@naver.com>
2024-06-09 19:07:28 +0900

    8000

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ async def read_memo_page():
 app.include_router(memo_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
